8218fb30c8871a8fb19f375d44ece07eac224a84
Sanya Nanda <sanya.nanda@gmail.com>
2021-05-10 16:32:46 +0530

    first React app

---
+++ monsters/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters:[
+        // {name: 'Frankenstein',
+        //   id: 'a1'
+        // },
+      ],
+      searchField: ''
+    };
+
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>response.json())
+    .then(users => this.setState({ monsters:users }))
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+  render(){
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    return(
+    <div className="App">
+      <h1>Monsters</h1>
+      <SearchBox 
+      placeholder = 'Search Monsters'
+      handleChange = {this.handleChange} />
+      <CardList monsters={filterMonsters}/>
+    </div>
+  );
+  }
+}
+
+export default App;
